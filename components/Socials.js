ca5283fd9379ebebcfba83f996d6f6257e28302e
HAI230 <139831312+HAI230@users.noreply.github.com>
2023-08-12 00:20:59 +0800

    Socials.js

--- components/Socials.js
+++ components/Socials.js
@@ -12,16 +12,16 @@ import {
 const Socials = () => {
   return (
     <div className='flex items-center gap-x-5 text-lg'>
-      <Link href={''} className='hover:text-accent transition-all duration-300'>
+      <Link href={'https://www.youtube.com/channel/UCINd3Og3tpbyPrkZS-nAAkw'} className='hover:text-accent transition-all duration-300'>
         <RiYoutubeLine />
       </Link>
       <Link href={''} className='hover:text-accent transition-all duration-300'>
         <RiFacebookLine />
       </Link>
-      <Link href={''} className='hover:text-accent transition-all duration-300'>
+      <Link href={'https://www.instagram.com/davin_jong72/'} className='hover:text-accent transition-all duration-300'>
         <RiInstagramLine />
       </Link>
-      <Link href={''} className='hover:text-accent transition-all duration-300'>
+      <Link href={'https://id.pinterest.com/davinjong01/'} className='hover:text-accent transition-all duration-300'>
         <RiPinterestLine />
       </Link>
     </div>
